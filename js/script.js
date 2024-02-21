deec04782fae7705877e50abb1f96f61ae43ed50
Pedro <phbr10.12@gmail.com>
2024-02-20 21:44:48 -0300

    menu hamburguer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+const menuHamburguer = document.getElementById("menu-hamburguer")
+const sideNav = document.getElementById("side-nav")
+const closeIcon = document.getElementById("close-icon")
 
+
+
+
+
+menuHamburguer.addEventListener("click", () => {
+  sideNav.style.display = "flex"
+  sideNav.style.animationName = "showMenu"
+})
+
+closeIcon.addEventListener("click", () => {
+  sideNav.style.display = "none"
+  sideNav.style.animationName = "closeMenu"
+})
