65e5e98efc70b14c597873b3d6877028fe3afe76
Pedro Henrique Admin <doctorfast10.12@hotmail.com>
2024-08-12 14:13:47 -0300

    fix: footer css

--- src/parts/footer/Footer.tsx
+++ src/parts/footer/Footer.tsx
@@ -2,7 +2,7 @@ import './footer.module.css'
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer style={{position: "absolute", bottom: 0, left: 0}}>
       <h1 style={{color: "#585858"}}>Copyright © 2024 <span style={{color: "#cecece"}}>{"< Pedro Barbosa />"}</span></h1>
     </footer>
   )
