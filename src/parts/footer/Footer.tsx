461709b2488ac30b679513e1c84866459f31b98d
Pedro <doctorfast10.12@hotmail.com>
2024-08-12 18:24:51 -0300

    footer css

--- src/parts/footer/Footer.tsx
+++ src/parts/footer/Footer.tsx
@@ -1,8 +1,8 @@
-import './footer.module.css'
+import styles from './footer.module.css'
 
 export const Footer = () => {
   return (
-    <footer style={{position: "absolute", bottom: 0, left: 0}}>
+    <footer className={styles.footer} style={{position: "absolute", bottom: 0, left: 0}}>
       <h1 style={{color: "#585858"}}>Copyright © 2024 <span style={{color: "#cecece"}}>{"< Pedro Barbosa />"}</span></h1>
     </footer>
   )
